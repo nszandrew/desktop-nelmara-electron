7462953601321fba0bd7aa86e8db78b115406f9f
nszandrew <nszandrew10@gmail.com>
2025-07-21 15:11:15 -0300

    feat: add auto-update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { autoUpdater } = require('electron-updater');
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -12,10 +13,39 @@ function createWindow() {
   });
 
   // Para desenvolvimento, carrega o frontend do Vite
-  win.loadURL('http://localhost:5173');
+  // Em produção, substitua por: win.loadFile(path.join(__dirname, 'dist', 'index.html'))
+  //win.loadURL('http://localhost:5173');
+  win.loadFile(path.join(__dirname, 'dist', 'index.html'));
+
+  autoUpdater.checkForUpdatesAndNotify();
 }
 
-app.whenReady().then(createWindow);
+autoUpdater.on('checking-for-update', () => {
+  console.log('Verificando atualizações...');
+});
+
+autoUpdater.on('update-available', () => {
+  console.log('Atualização disponível!');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  console.log('Atualização baixada. Instalando...');
+  autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('error', (err) => {
+  console.error('Erro ao atualizar:', err);
+});
+
+app.whenReady().then(() => {
+  autoUpdater.setFeedURL({
+    provider: 'github',
+    owner: 'nszandrew', 
+    repo: 'https://github.com/nszandrew/desktop-nelmara-electron' 
+  });
+
+  createWindow();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
